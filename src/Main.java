28bebb04167c1179b398fedeff681a3ff4b3b023
profricardounifil <100451048+profricardounifil@users.noreply.github.com>
2025-11-05 18:09:14 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         VetorOps vetorOps = new VetorOps();
         vetorOps.preencherVetor();
-        vetorOps.selectionSort();
+        vetorOps.mergeSort(0, vetorOps.vetor.length-1);
         vetorOps.exibirVetor();
     }
 }
